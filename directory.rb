b576f1beb3c2359c025761b6a172cfd5b142a196
Manisha Patel <ma9shap.gmail.com>
2019-05-10 14:13:15 +0100

    student list populated manually

--- directory.rb
+++ directory.rb
@@ -1,19 +1,23 @@
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "______________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of students
-print "overall, we have "
-# it'd important that print () doesn't add new line characters
-print 11
-puts " great students"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print (students)
+print_footer(students)
