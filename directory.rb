ff68a11891f319c2bdad03c78aacb55c53db7700
Manisha Patel <ma9shap.gmail.com>
2019-05-10 15:36:53 +0100

    print names below 12 chracters

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each_with_index do |x, index| 
-    if x[:name][0] == "a"
+    if x[:name][0] == "a" && x[:name].length < 12
     puts "#{index + 1}) #{x[:name]} (#{x[:cohort]} cohort)"
     end
   end
